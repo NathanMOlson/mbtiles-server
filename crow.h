a242e738066f49ecdc113e07d7bbbc44857f64af
Nathan <nathanmolson@gmail.com>
2024-11-11 14:24:42 -0800

    fix compile

--- crow.h
+++ crow.h
@@ -3285,7 +3285,7 @@ namespace crow
 
         boost::asio::io_service& get_io_service()
         {
-            return socket_.get_io_service();
+            return (boost::asio::io_context&)socket_.get_executor().context();
         }
 
         tcp::socket& raw_socket()
